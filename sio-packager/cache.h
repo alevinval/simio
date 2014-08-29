eee287e4404eab3a4268eaec3c17b748dcb748d2
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2014-08-29 19:43:18 +0200

    ( resolve #9 ) Provide the template for caches of values. The cache must be sorted after the loading of values. Not using the unordered_map, because ... no.

---
+++ sio-packager/cache.h
@@ -0,0 +1,33 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <vector>
+#include <string>
+
+template <class Entry>
+class Cache {
+    std::vector<Entry> entries_;
+public:
+
+    void sort() {
+        std::sort (entries_.begin(), entries_.end(), [] (const Entry &e1, const Entry &e2) { return e1 < e2; });
+    }
+
+    void add(Entry &entry) {
+        entries_.push_back (entry);
+    }
+
+    bool search(const Entry &entry) {
+        return std::binary_search (entries_.begin(), entries_.end(), entry, [] (const Entry &e1, const Entry &e2) { return e1 < e2; });
+    }
+
+    std::vector<Entry> &entries() {
+        return entries_;
+    }
+
+    int size() const {
+        return entries_.size();
+    }
+};
+
+#endif //CACHE_H
